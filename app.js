28c7ddaa53ca1e7f51d078db665eba6d1a1b4e0d
TrevorMar <38366091+TrevorMar@users.noreply.github.com>
2019-06-03 20:59:15 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+var aText = new Array(
+"Oops... You shouldn't have seen this.",
+"",
+"Trevor is currently working on creating his new website.",
+"",
+"Check back soon!",
+"",
+"-Trevor :)"
+);
+var iSpeed = 100; 
+var iIndex = 0; 
+var iArrLength = aText[0].length; 
+var iScrollAt = 20; 
+ 
+var iTextPos = 0; 
+var sContents = ''; 
+var iRow; 
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
